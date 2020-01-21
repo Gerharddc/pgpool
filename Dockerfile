5d2f1b963b3baa8c64871fa2f27f29374a162419
Gerhard de Clercq <gerharddeclercq@outlook.com>
2020-01-21 22:18:07 +0200

    Updated conf file locations for debian

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM debian:stable-slim
 RUN apt-get update && apt-get -y install pgpool2 gettext
 RUN mkdir /var/run/pgpool/
 
-COPY ./conf /etc/pgpool
+COPY ./conf /etc/pgpool2
 COPY entrypoint.sh .
 
 EXPOSE 5432
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@
 pg_md5 -m -u ${USERNAME} ${PASSWORD}
 
 # Setup the config files
-envsubst < /etc/pgpool/pgpool.template.conf > /etc/pgpool/pgpool.conf
+envsubst < /etc/pgpool2/pgpool.template.conf > /etc/pgpool2/pgpool.conf
 
 exec "$@"
